d3f1912faf131d0a9c8968babaa9898eadfebc40
Alok <alokchoudhary162@gmail.com>
2018-01-17 08:40:41 +0530

    Changes for contact on social link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,9 @@ export class Footer extends Component {
       <div> <div className="footer">
       <div className="medium-7 columns">
         <h3>Contact Me</h3>
-       
+        <a href="https://www.linkedin.com/in/alok-choudhary-1b028310a/" class="btn btn-block btn-social btn-linkedin">
+         <span class="fa fa-linkedin"></span> Sign in with Twitter
+        </a>
       </div>
     </div></div>
     );
