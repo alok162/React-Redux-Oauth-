bc12bc93564337fd94067b0d4fefcdc00139b1e0
Alok <alokchoudhary162@gmail.com>
2018-01-28 12:46:44 +0530

    changed app  credential

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import firebase from 'firebase';
 
 // Firebase database configuration
 const config = {
-  apiKey: "AIzaSyAjvY8OVPoYAFtVKMNq35ttUBm2xsyQQsw",
-  authDomain: "grow-fit-project.firebaseapp.com",
-  databaseURL: "https://grow-fit-project.firebaseio.com",
-  projectId: "grow-fit-project",
-  storageBucket: "grow-fit-project.appspot.com",
-  messagingSenderId: "862272674337"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
 const app = firebase.initializeApp(config)
